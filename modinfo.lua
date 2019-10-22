79bacaacae8147cd17f5dda31852c653a84db293
OroshiX <oroshix@users.noreply.github.com>
2019-10-22 23:53:20 +0200

    As published as first version

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Lantern glitch"
 
-description = "Brings back the lantern glitch."
+description = "Lantern glitch is back!"
 author = "MrOroshiX"
 version = "1.0.0"
 forumthread = ""
@@ -11,22 +11,8 @@ dont_starve_compatible = true
 reign_of_giants_compatible = true
 hamlet_compatible = true
 shipwrecked_compatible = true
-server_filter_tags = {"lantern bug","lantern","infinite lantern"}
+
+server_filter_tags = {"lantern bug","lantern","infinite lantern","lantern glitch", "glitch","infinite light"}
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
-
-configuration_options =
-{
-	{
-		name = "isLit",
-		label = "lanterns on/off",
-        hover = "Change whether you want to keep bugged lanterns on or off.",
-		options =
-	{
-		{description = "On", data = "InfiniteLanternOn", hover = ""},
-		{description = "Off", data = "InfiniteLanternOff", hover = ""},
-	},
-		default = "Off",
-	}
-}
--- modmain.lua
+++ modmain.lua
@@ -1,67 +1,135 @@
-local assets =
-{
-    Asset("ANIM", "anim/lantern.zip"),
-    Asset("ANIM", "anim/swap_lantern.zip"),
-}
-
-local function newLightup(inst)
-    print("is Equipped?? -- in lightup ")
-    print(inst.components.equippable:IsEquipped())
-    if inst.components.fueled and inst.components.equippable:IsEquipped() then
-        inst.components.fueled:StartConsuming()
-    end
-
-    inst.Light:Enable(true)
-    inst.components.floatable:UpdateAnimations("idle_on_water", "idle_on")
-
-    if inst.components.equippable:IsEquipped() then
-        inst.components.inventoryitem.owner.AnimState:OverrideSymbol("swap_object", "swap_lantern", "swap_lantern_on")
-        inst.components.inventoryitem.owner.AnimState:Show("LANTERN_OVERLAY")
-    end
-    inst.components.machine.ison = true
-
-    inst.SoundEmitter:PlaySound("dontstarve/wilson/lantern_on")
-    inst.SoundEmitter:PlaySound("dontstarve/wilson/lantern_LP", "loop")
-
-    inst.components.inventoryitem:ChangeImageName("lantern_lit")
-    --    if InfiniteLanternOn then
-    --    end
-end
-
 local function newTurnon(inst)
-    print("TURN ON!!!!!!!!")
-
     if not inst.components.fueled:IsEmpty() then
         if not inst.components.machine.ison then
-            newLightup(inst)
+            if inst.components.fueled then
+                inst.components.fueled:StartConsuming()
+            end
+            inst.Light:Enable(true)
+            inst.components.floatable:UpdateAnimations("idle_on_water", "idle_on")
+
+            if inst.components.equippable:IsEquipped() then
+                inst.components.inventoryitem.owner.AnimState:OverrideSymbol("swap_object", "swap_lantern", "swap_lantern_on")
+                inst.components.inventoryitem.owner.AnimState:Show("LANTERN_OVERLAY")
+            end
+            inst.components.machine.ison = true
+
+            inst.SoundEmitter:PlaySound("dontstarve/wilson/lantern_on")
+            inst.SoundEmitter:PlaySound("dontstarve/wilson/lantern_LP", "loop")
+
+            inst.components.inventoryitem:ChangeImageName("lantern_lit")
         end
     end
 end
 
-local function newOnLoad(inst, data)
-    print("ON LOAD!!!!!!!!")
-    if inst.components.machine and inst.components.machine.ison then
-        newLightup(inst)
-    else
-        turnoff(inst)
+local function newTurnoff(inst)
+    if inst.components.fueled then
+        inst.components.fueled:StopConsuming()
     end
+
+    inst.Light:Enable(false)
+    inst.components.floatable:UpdateAnimations("idle_off_water", "idle_off")
+
+    if inst.components.equippable:IsEquipped() then
+        inst.components.inventoryitem.owner.AnimState:OverrideSymbol("swap_object", "swap_lantern", "swap_lantern_off")
+        inst.components.inventoryitem.owner.AnimState:Hide("LANTERN_OVERLAY")
+    end
+
+    inst.components.machine.ison = false
+
+    inst.SoundEmitter:KillSound("loop")
+    inst.SoundEmitter:PlaySound("dontstarve/wilson/lantern_off")
+
+    inst.components.inventoryitem:ChangeImageName("lantern")
+end
+
+local function newOnLoad(inst, data)
+    if inst.components.machine and inst.components.machine.ison then
+        newTurnon(inst)
+    else
+        newTurnoff(inst)
+    end
+end
+
+local function newOndropped(inst)
+    newTurnoff(inst)
+    newTurnon(inst)
 end
 
 local function newOnpickup(inst)
-    print("ON PICK UP!!!!!!!!")
-    newLightup(inst)
+    newTurnon(inst)
 end
 
-local function newTakeFuel(inst)
-    print("TAKE FUEL!!!!!!!!")
-    if inst.components.equippable and inst.components.equippable:IsEquipped() then
-        newLightup(inst)
+local function newOnputininventory(inst)
+    newTurnoff(inst)
+end
+
+--
+local function newOnequip(inst, owner)
+    owner.AnimState:Show("ARM_carry")
+    owner.AnimState:Hide("ARM_normal")
+    owner.AnimState:OverrideSymbol("lantern_overlay", "swap_lantern", "lantern_overlay")
+
+    if inst.components.fueled:IsEmpty() then
+        owner.AnimState:OverrideSymbol("swap_object", "swap_lantern", "swap_lantern_off")
+        owner.AnimState:Hide("LANTERN_OVERLAY")
+    else
+        owner.AnimState:OverrideSymbol("swap_object", "swap_lantern", "swap_lantern_on")
+        owner.AnimState:Show("LANTERN_OVERLAY")
     end
+    newTurnon(inst)
+end
+
+local function newOnunequip(inst, owner)
+    owner.AnimState:Hide("ARM_carry")
+    owner.AnimState:Show("ARM_normal")
+    owner.AnimState:ClearOverrideSymbol("lantern_overlay")
+    owner.AnimState:Hide("LANTERN_OVERLAY")
+end
+
+local function newNofuel(inst)
+    local owner = inst.components.inventoryitem and inst.components.inventoryitem.owner
+    if owner then
+        owner:PushEvent("torchranout", { torch = inst })
+    end
+
+    newTurnoff(inst)
+end
+
+local function newTakefuel(inst)
+    if inst.components.equippable and inst.components.equippable:IsEquipped() then
+        newTurnon(inst)
+    end
+end
+local function lerp(pos1, pos2, perc)
+    return (1-perc)*pos1 + perc*pos2 -- Linear Interpolation
+end
+
+local function newFuelupdate(inst)
+    local fuelpercent = inst.components.fueled:GetPercent()
+    inst.Light:SetIntensity(lerp(0.4, 0.6, fuelpercent))
+    inst.Light:SetRadius(lerp(3, 5, fuelpercent))
+    inst.Light:SetFalloff(.9)
 end
 
 AddPrefabPostInit("lantern", function(inst)
-    inst.components.machine.turnonfn = newTurnon
-    inst.components.fueled.ontakefuelfn = newTakeFuel
+    inst.components.fueled:SetUpdateFn(newFuelupdate)
+    inst.components.fueled:SetDepletedFn(newNofuel)
+    inst.components.inventoryitem:SetOnDroppedFn(newOndropped)
+    inst.components.equippable:SetOnEquip(newOnequip)
     inst.OnLoad = newOnLoad
     inst.components.inventoryitem:SetOnPickupFn(newOnpickup)
+    inst.components.inventoryitem:SetOnPutInInventoryFn(newOnputininventory)
+    inst.components.equippable:SetOnUnequip(newOnunequip)
+    inst.components.fueled.ontakefuelfn = newTakefuel
+    inst.components.machine.turnofffn = newTurnoff
+    inst.components.machine.turnonfn = newTurnon
+    inst:ListenForEvent("startrowing", function(inst, data)
+        --print("start rowing!!")
+        newTurnoff(inst)
+    end, inst)
+    inst:ListenForEvent("stoprowing", function(inst, data)
+        --print("stop rowing!!")
+        newTurnon(inst)
+    end, inst)
+
 end)
